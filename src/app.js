dd3e4b70cad92dbf9e78020de361980d9531e350
karen <karenkuruvillajoseph@gmail.com>
2022-08-24 17:45:20 +0530

    Init

---
+++ src/app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Brian",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Brian",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "Help About Me",
+    title: "Help",
+    name: "Brian",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Must send an address",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "Must provide search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 Help",
+    name: "Brian",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (erq, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Brian",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server up on port 3000");
+});
